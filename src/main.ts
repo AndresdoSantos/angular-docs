214afe2e862bf46b8bb9e421d4eb95be4f45307e
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2023-03-27 10:29:57 -0300

    feat: create my first component

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import '@angular/compiler'
+import 'zone.js'
+import { bootstrapApplication } from '@angular/platform-browser'
+
+import { AppComponent } from './components/app.component'
+
+bootstrapApplication(AppComponent)
